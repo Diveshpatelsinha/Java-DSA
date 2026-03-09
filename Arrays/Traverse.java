f1fd2e384be707cf21c5364cb9ee388a1edaa0cc
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-09 12:52:32 +0530

    Traverse the array forward and backward

---
+++ Arrays/Traverse.java
@@ -0,0 +1,16 @@
+
+
+
+public class Traverse {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5,6};
+
+        // for(int i = 0; i<array.length; i++){
+        //     System.out.println(array[i]);
+        // }
+        for(int j = array.length-1; j>=0; j--){
+             System.out.println(array[j]);
+        }
+        // System.out.println(array.length);
+    }
+}

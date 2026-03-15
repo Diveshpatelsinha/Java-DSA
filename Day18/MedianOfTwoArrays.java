eec44d208b8023baefea14dc9ef62d496b7b9b0d
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-15 18:19:21 +0000

    median of the two arrays

---
+++ Day18/MedianOfTwoArrays.java
@@ -0,0 +1,48 @@
+public class MedianOfTwoArrays{
+
+
+    public static double findmedian(int[] a, int[] b){
+        int n = a.length + a.length;
+        int[] merge = new int[n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j])
+                merge[k++] = a[i++];
+            else
+                merge[k++] = b[j++];
+        }
+
+        while(i<a.length){
+             merge[k++] = a[i++];
+        }
+
+         while(j<b.length){
+             merge[k++] = b[j++];
+        }
+
+        if(i%2==0){
+            return (merge[n/2-1]+merge[n/2])/2;
+        }
+        else{
+             return merge[n/2];
+            }
+
+            // for(int x : merge){
+            //      System.out.print(x + " ");
+            // }
+
+    }
+
+
+    public static void main(String[] args){
+        int[] a = {1,4,5,3};
+        int[] b = {8,3,9,0};
+
+        System.out.println(findmedian(a,b));
+       
+        }
+    }

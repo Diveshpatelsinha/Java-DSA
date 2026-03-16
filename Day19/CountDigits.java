15bf55ffc2b3c448d7f8cb172149f974fbcda43f
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-16 18:11:10 +0000

    count digits in a number

---
+++ Day19/CountDigits.java
@@ -0,0 +1,16 @@
+public class CountDigits{
+
+    public static void main(String[] args) {
+         int num = 123456;
+    int count = 0;
+
+    while (num!=0) {
+        num/=10;
+        count++;
+    }
+
+    System.out.println(count);
+    }
+   
+
+}

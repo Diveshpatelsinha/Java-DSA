ef20bcff78fd2038fd47fd465d016bf492ad8da8
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-24 13:51:03 +0530

    Reverse word order

--- Day26/ReverseSentence.java
+++ Day26/ReverseSentence.java
@@ -22,3 +22,18 @@ public class ReverseSentence {
         System.out.println(rev);
     }
 }
+
+//method 2
+public class ReverseSentence{
+    public static void main(String[] args){
+     String s = "Sky is blue";
+      String[] word = s.split(" ");
+        String rev = "";
+      for(int i = word.length-1;i>=0;i--){
+       rev+= word[i]+" ";
+      }
+
+      System.out.println(rev);
+    }
+     
+}

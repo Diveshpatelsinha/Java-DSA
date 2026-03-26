5859c946ed7215d0a71a5b34d37c7e0766c1f474
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-26 13:26:15 +0530

    First Unique Character in a String

---
+++ Day28/Leetcode387.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[128];
+
+        for(char c : s.toCharArray()){
+            freq[c]++;
+
+        }
+        int index = 0;
+        boolean r = false;
+        for(int i = 0; i<s.length(); i++){
+            if(freq[s.charAt(i)]==1){
+                return i;
+            }
+        }
+       
+            return -1;
+       
+
+    }
+}

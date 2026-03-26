a5c9dfd25bb6ede5904024f3dbc5f609735d4c19
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-26 13:13:03 +0530

    Leetcode problem 69 which is square of a num

---
+++ Day27/Leetcode69.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        // if(x<2) return x;
+        // int left = 1;
+        // int right = x/2;
+        // int ans = 0;
+
+        // while(left<=right){
+        //     int mid = left + (right-left)/2;
+
+        //     if((long)mid*mid == x){
+        //         return mid;
+        //     }
+
+        //     if((long)mid*mid<x){
+        //         ans = mid;
+        //         left = mid+1;
+        //     }else{
+        //         right = mid - 1;
+        //     }
+        // }
+        // return ans;
+
+        
+    if (x == 0) return 0;
+
+    long r = x;
+
+    while (r * r > x) {
+        r = (r + x / r) / 2;
+    }
+
+    return (int) r;
+
+    }
+}
